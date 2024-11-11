39588f39656b400076e407832b327717909d7dd1
Hifumi090 <zhanglantian1996@gmail.com>
2024-11-11 18:04:26 +0800

    Init

---
+++ Packages/cn.etetet.statesync/Editor/InitHelper.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ET
+{
+    public static class InitHelper
+    {
+        [MenuItem("ET/StateSync/Init")]
+        public static void Init()
+        {
+            HybridCLREditor.Init();
+            
+            // ExcelEditor.Init();
+            EditorApplication.ExecuteMenuItem("ET/Excel/ExcelExporter");
+            
+            ProtoEditor.Init();
+            
+            LoaderEditor.Init();    
+            
+            AssetDatabase.Refresh();
+            
+            Debug.Log("Init finish!");
+        }
+    }
+}
