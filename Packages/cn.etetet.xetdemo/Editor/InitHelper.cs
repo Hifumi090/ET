eef52721fcb75ac7529dfe65f5ae0b0537c1c194
Hifumi090 <zhanglantian1996@gmail.com>
2024-11-11 18:28:57 +0800

    XET FairyGUI

---
+++ Packages/cn.etetet.xetdemo/Editor/InitHelper.cs
@@ -0,0 +1,26 @@
+
+using UnityEditor;
+using UnityEngine;
+
+namespace ET
+{
+    public static class InitHelper
+    {
+        [MenuItem("ET/XET/Init", false, 1)]
+        public static void Init()
+        {
+            HybridCLREditor.Init();
+
+            // ExcelEditor.Init();
+            EditorApplication.ExecuteMenuItem("ET/Excel/ExcelExporter");
+
+            ProtoEditor.Init();
+
+            LoaderEditor.Init();    
+
+            AssetDatabase.Refresh();
+
+            Debug.Log("Init finish!");
+        }
+    }
+}
